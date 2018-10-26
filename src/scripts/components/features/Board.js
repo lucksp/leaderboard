b78424ad417bfd80234159fa0389fe08e19420c3
Phil Lucks <philip.lucks@snapengage.com>
2018-10-25 22:11:41 -0600

    better editing

--- src/scripts/components/features/Board.js
+++ src/scripts/components/features/Board.js
@@ -1,5 +1,5 @@
 // react imports
-import React, { Component } from "react";
+import React from "react";
 import { Table, Container } from "reactstrap";
 import PlayerInput from "./PlayerInput";
 import TableRow from "./TableRow";
@@ -7,54 +7,38 @@ import TableRow from "./TableRow";
 // redux/state imports
 import { connect } from "react-redux";
 
-class Board extends Component {
-  state = {
-    editWinnings: null
-  };
-
-  toggleState = (e, id) => {
-    this.setState({ editWinnings: !this.state.editWinnings ? id : null });
-  };
-
-  render() {
-    return (
-      <Container id="board">
-        <div className="leaderboard">
-          <Table striped>
-            <thead>
-              <tr>
-                <td colSpan="4" className="table-header-add">
-                  <div id="board-new">Add a new Player</div>
-                  <PlayerInput />
-                </td>
-              </tr>
-              <tr />
-              <tr>
-                <th xs="1">#</th>
-                <th xs="4">Name</th>
-                <th xs="4">Winnings</th>
-                <th xs="3">Country</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.keys(this.props.playerData).map((item, i) => {
-                return (
-                  <TableRow
-                    data={this.props.playerData[item]}
-                    idx={i + 1}
-                    key={i}
-                    handleEditToggle={this.toggleState}
-                    editStatus={this.state.editWinnings}
-                  />
-                );
-              })}
-            </tbody>
-          </Table>
-        </div>
-      </Container>
-    );
-  }
-}
+const Board = props => {
+  return (
+    <Container id="board">
+      <div className="leaderboard">
+        <Table striped>
+          <thead>
+            <tr>
+              <td colSpan="4" className="table-header-add">
+                <div id="board-new">Add a new Player</div>
+                <PlayerInput />
+              </td>
+            </tr>
+            <tr />
+            <tr>
+              <th xs="1">#</th>
+              <th xs="4">Name</th>
+              <th xs="4">Winnings</th>
+              <th xs="3">Country</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(props.playerData).map((item, i) => {
+              return (
+                <TableRow data={props.playerData[item]} idx={i + 1} key={i} />
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+    </Container>
+  );
+};
 
 function mapStateToProps(state) {
   return {
--- src/scripts/components/features/EditableField.js
+++ src/scripts/components/features/EditableField.js
@@ -2,44 +2,116 @@ import React, { Component } from "react";
 import { InputGroup, Input } from "reactstrap";
 import { updateWinnings } from "../../redux/actions/common";
 
+import check from "../../../../public/assets/img/icon-check.png";
+import edit from "../../../../public/assets/img/icon-edit.png";
+import cancel from "../../../../public/assets/img/icon-cancel.png";
+
 // redux/state imports
 import { connect } from "react-redux";
 
 class EditableField extends Component {
   editField = React.createRef();
-  state = { value: null };
+  state = { value: null, isEditing: false };
 
   componentDidMount() {
-    this.editField.focus();
+    if (this.props.originalValue) {
+      this.setState({ value: this.props.originalValue });
+    }
   }
-  handleChange = e => {
-    this.setState({ value: this.editField.value });
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.isEditing) {
+      this.editField.current.focus();
+    }
+  }
+
+  toggleEditing = () => {
+    this.setState({ isEditing: !this.state.isEditing, value: "" });
   };
 
+  handleChange = e => {
+    e.preventDefault();
+
+    let tempValue = this.editField.current.innerHTML;
+    //check for only numbers
+    if (!tempValue.match(/^[0-9]*$/)) {
+      // TODO - return better error message
+      return this.toggleEditing();
+    }
+    if (e.key === "Enter") {
+      this.handleSave(e);
+    } else if (e.key === "Escape" || e.type === "blur") {
+      this.toggleEditing();
+      tempValue = this.props.originalValue;
+    }
+
+    this.setState({ value: tempValue });
+  };
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // to prevent edit cursor from going to beginning
+    if (nextState.value == this.editField.current.innerHTML) {
+      return false;
+    }
+    return true;
+  }
+
   handleSave = e => {
+    if (this.state.value === "") return this.toggleEditing();
     this.props.updateWinnings(this.state.value, this.props.userId);
-    this.setState({ value: null }, () => {
-      this.props.handleEditToggle(e, null);
-    });
+    this.toggleEditing();
   };
 
   render() {
     return (
-      <InputGroup className="edit-winning">
-        <Input
-          onKeyUp={e => {
+      <React.Fragment>
+        <span
+          ref={this.editField}
+          className="edit-winning"
+          suppressContentEditableWarning={true}
+          contentEditable={this.state.isEditing}
+          onInput={this.handleChange}
+          onKeyDown={e => {
             if (e.key === "Enter") {
               this.handleSave(e);
-            } else if (e.key === "Escape") {
-              this.props.handleEditToggle(e, this.props.userId);
+              return;
             }
           }}
-          onChange={this.handleChange}
-          innerRef={input => (this.editField = input)}
-          type="number"
-          placeholder="Enter new value"
-        />
-      </InputGroup>
+          onBlur={this.handleChange}
+        >
+          {this.state.isEditing ? this.state.value : this.props.originalValue}
+        </span>
+
+        {this.state.isEditing ? (
+          <div className="edit-toolbar">
+            <div className="winnings-edit-icon check">
+              <img
+                src={check}
+                onClick={e => {
+                  this.toggleEditing(e, this.props.userId);
+                }}
+              />
+            </div>
+            <div className="winnings-edit-icon cancel">
+              <img
+                src={cancel}
+                onClick={e => {
+                  this.toggleEditing(e, this.props.userId);
+                }}
+              />
+            </div>
+          </div>
+        ) : (
+          <div className="winnings-edit-icon edit">
+            <img
+              src={edit}
+              onClick={e => {
+                this.toggleEditing(e, this.props.userId);
+              }}
+            />
+          </div>
+        )}
+      </React.Fragment>
     );
   }
 }
