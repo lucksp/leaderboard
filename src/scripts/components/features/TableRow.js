4e08d9194a875ff3c00ec8909128aa10a8310e63
Phil Lucks <philip.lucks@snapengage.com>
2018-10-25 22:11:41 -0600

    better editing

--- src/scripts/components/features/TableRow.js
+++ src/scripts/components/features/TableRow.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
 import EditableField from "./EditableField";
 
-import check from "../../../../public/assets/img/icon-check.png";
-import edit from "../../../../public/assets/img/icon-edit.png";
-
 const TableRow = (
-  { data, handleEditToggle, editStatus, idx } = {
+  { data, handleEditToggle, idx } = {
     ...this.props
   }
 ) => {
@@ -14,25 +11,11 @@ const TableRow = (
       <th scope="row">{idx}</th>
       <td className="player-name">{data.name}</td>
       <td className="player-winnings">
-        {editStatus === data.id ? (
-          <EditableField
-            handleEditToggle={e => {
-              handleEditToggle(e, data.id);
-            }}
-            userId={data.id}
-            originalValue={data.winnings}
-          />
-        ) : (
-          `$${data.winnings}`
-        )}
-        <div className={`name-edit-icon ${editStatus ? "check" : "edit"}`}>
-          <img
-            src={editStatus ? check : edit}
-            onClick={e => {
-              handleEditToggle(e, data.id);
-            }}
-          />
-        </div>
+        <EditableField
+          handleEdit={handleEditToggle}
+          userId={data.id}
+          originalValue={data.winnings}
+        />
       </td>
       <td className="player-country">
         <span className="table-data-emoji">{data.emoji}</span>
